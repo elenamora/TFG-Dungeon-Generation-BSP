de494b99754730ab352d2f9b7783c71f3ee69142
Elena Mora Bajén <elenamorabajen@MacBook-Pro-de-Elena.local>
2021-05-05 23:53:23 +0200

    menuAdapt

---
+++ Assets/Scripts/Adaptative/UI/WeaponSelectionAdapt.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WeaponSelectionAdapt : MonoBehaviour
+{
+    private int index = 0;
+
+    public void ChooseWeapon()
+    {
+        PlayerPrefs.SetInt("WeaponSelected", index);
+        SceneManager.LoadScene("BSPAdapt");
+    }
+
+    public void SelectSword()
+    {
+        index = 0;
+        ChooseWeapon();
+    }
+
+    public void SelectAxe()
+    {
+        index = 2;
+        ChooseWeapon();
+    }
+
+    public void SelectHammer()
+    {
+        index = 1;
+        ChooseWeapon();
+    }
+
+}
