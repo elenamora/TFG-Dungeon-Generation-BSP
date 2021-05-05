e1e81ca017b45c6b5971b3cb599759c757580ce4
Elena Mora Bajén <elenamorabajen@MacBook-Pro-de-Elena.local>
2021-05-06 00:07:43 +0200

    menuAdapt

---
+++ Assets/Scripts/Adaptative/UI/PauseManagerAdapt.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManagerAdapt : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MenuAdapt");
+        Time.timeScale = 1f;
+    }
+}
